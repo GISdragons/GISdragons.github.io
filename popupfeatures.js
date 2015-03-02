019b98ea3b864d3c67439877ac2302f9420411b0
pmerrell <prestonmerrell@gmail.com>
2015-03-02 11:21:09 -0700

    Update popupfeatures.js

--- popupfeatures.js
+++ popupfeatures.js
@@ -50,11 +50,10 @@ var map = new ol.Map({
   overlays: [overlay],
   target: 'map',
   view: new ol.View({
-    center: [0, 0],
-    zoom: 2
-  })
+	center: ol.proj.transform([-81.36191579883884, 41.6251840640563], 'EPSG:4326', projection),
+	zoom: 5
+	})
 });
-
 
 map.on('click', function(evt) {
   //try to destroy it before doing anything else...s
