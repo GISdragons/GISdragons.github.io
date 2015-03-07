bc75861b4e5a91c7598b4bddbeb63448db2d0a8c
pmerrell <prestonmerrell@gmail.com>
2015-03-06 17:38:27 -0700

    popups appear!
    put in popups

--- map.js
+++ map.js
@@ -45,23 +45,23 @@ var map = new ol.Map({
 	})
 });
 
-//  map.on('click', function(evt) {
+ map.on('click', function(evt) {
   
 //   //Try to get a feature at the point of interest
-//   var feature = map.forEachFeatureAtPixel(evt.pixel,
-// 	  function(feature, layer) {
-// 		return feature;
-// 	  });
+   var feature = map.forEachFeatureAtPixel(evt.pixel,
+ 	  function(feature, layer) {
+ 		return feature;
+ 	  });
 	  
-//   //if we found a feature then create and show the popup.
-//   if (feature) {
-// 	var geometry = feature.getGeometry();
-// 	var coord = geometry.getCoordinates();
-// 	overlay.setPosition(coord);
-// 	var displaycontent = feature.get('description');
-// 	content.innerHTML = displaycontent;
-//   }
-// });
+ //if we found a feature then create and show the popup.
+  if (feature) {
+ 	var geometry = feature.getGeometry();
+ 	var coord = geometry.getCoordinates();
+ 	overlay.setPosition(coord);
+ 	var displaycontent = feature.get('description');
+ 	content.innerHTML = displaycontent;
+   }
+    });
 
 function dropdown_clicked(ID) {
 	myFeature = JSLife.getSource().getFeatures()[ID];
