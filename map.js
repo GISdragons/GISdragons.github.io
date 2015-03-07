02f644be8925bcd104da5bef89abff1e5add4bcd
sduncan4 <StephenDuncan64@gmail.com>
2015-03-07 08:59:59 -0700

    other commented out stuff
    the pop up functions like overlay and closer were commented out. I undid that.

--- map.js
+++ map.js
@@ -6,17 +6,17 @@ var closer = document.getElementById('popup-closer');
 
 /** Adding a click handler to hide the popup
     just in case there is one already existing */
-/**closer.onclick = function() {
+closer.onclick = function() {
 	overlay.setPosition(undefined);
 	closer.blur();
 	return false;
 };
-*/
+
 /** Adding elements that make the pop-up */
-/**var overlay = new ol.Overlay({
+var overlay = new ol.Overlay({
 	element: container
 )};
-*/
+
 /** Here we are declaring the projection object for Web Mercator */
 var projection = ol.proj.get('EPSG:3857');
 
@@ -48,7 +48,7 @@ var map = new ol.Map({
  map.on('click', function(evt) {
   
 //   //Try to get a feature at the point of interest
-   var feature = map.forEachFeatureAtPixel(evt.pixel,
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
  	  function(feature, layer) {
  		return feature;
  	  });
