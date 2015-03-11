7cdd315c28c653e487ba08a135d37f98b32df0f3
pmerrell <prestonmerrell@gmail.com>
2015-03-11 13:04:15 -0600

    Update map.js

--- map.js
+++ map.js
@@ -86,7 +86,7 @@ function showData() {
 	midCoordIndex = parseInt(myCoords.length/2);
 	midCoord = myCoords[midCoordIndex];
 	myView.setCenter(midCoord);
-	myView.setZoom(5);
+	myView.setZoom(10);
 	JSLife.getSource().getFeatures()[13]
 	overlay.setPosition(midCoord);
 	var displaycontent = myFeature.get('description');
