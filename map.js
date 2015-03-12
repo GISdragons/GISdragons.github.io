d34d197bc3a451869b6c8a916ae8b67e1427068e
pmerrell <prestonmerrell@gmail.com>
2015-03-12 09:56:27 -0600

    Update map.js

--- map.js
+++ map.js
@@ -68,7 +68,7 @@ var map = new ol.Map({
 function clicked(ID) {
 	myFeature = JSLife.getSource().getFeatures()[ID];
 	myCoords = myFeature.getGeometry().getCoordinates();
-	myCoords[1] -= 600
+	myCoords[1] += 600
 	myView = map.getView();
 	myView.setCenter(myCoords);
 	myView.setZoom(15);
